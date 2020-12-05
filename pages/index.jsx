6c97c4da8ed97422a2d89ce9ddd2fc4d9ef6690a
surreptus <47543507+surreptus@users.noreply.github.com>
2020-12-04 18:10:56 -0800

    feat: change title for deploy

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Test
         </h1>
 
         <p className={styles.description}>
