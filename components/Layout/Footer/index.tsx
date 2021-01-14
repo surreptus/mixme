bb4a56a38758c3c59a354ddb14e0b2f18cec5b65
Amelia Brown <amelia.brown389@gmail.com>
2021-01-13 21:52:01 -0800

    feat: style footer

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import { Box, Flex, Text, Link, HStack} from '@chakra-ui/react'
+
 export default function Footer () {
   return (
-    <footer>
-      hello footer
-    </footer>
+    <Box mt={5}>
+      <Flex my={2} justify={'center'}>
+        <Text>mixme, a cocktail journal</Text>
+      </Flex>
+      <Flex justify={'center'}>
+        <HStack spacing={3}>
+          <Text>© {new Date().getFullYear()}</Text>
+          <Link href='https://github.com/surreptus/mixme' isExternal >Contribute</Link>
+        </HStack>
+      </Flex>
+    </Box>
   )
 }
